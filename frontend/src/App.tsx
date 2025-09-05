f0d93178219224f08cf87ad29cfbe76f236110d1
57ajay <57ajay.u@gmail.com>
2025-09-05 18:14:38 +0530

    frontend init with tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import _React from "react";
+
+function App() {
+    return (
+        <>
+            <h1 className="font-bold text-green-700">HI from AJAY.</h1>
+        </>
+    );
+}
+
+export default App;
